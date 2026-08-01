0281fa0c586c19bd9e686b841a2b4637cb79a39f
vasavi reddy <vasavireddy2006@gmail.com>
2026-08-01 16:31:40 +0530

    Change database connection string format

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
 #DATABASE_URL = "mysql+pymysql://root:Vasavi%40572@localhost:3306/restaurant_db"
-DATABASE_URL ="mysql://avnadmin:AVNS_mw4f0Fy7CB9EF4UP2TI@vasavi-project-vasavireddy2006-4319.g.aivencloud.com:23340/defaultdb?ssl-mode=REQUIRED"
+DATABASE_URL ="mysql+pymysql://avnadmin:AVNS_mw4f0Fy7CB9EF4UP2TI@vasavi-project-vasavireddy2006-4319.g.aivencloud.com:23340/defaultdb?ssl-mode=REQUIRED"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
